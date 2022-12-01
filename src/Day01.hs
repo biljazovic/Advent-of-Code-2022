3ccf0c9ca66cab93101e704768f49a03e89bf8e2
Bruno Iljazovic <bruno.iljazovic@gmail.com>
2022-12-01 13:38:09 +0100

    .

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,9 +2,8 @@ module Day01 (main01) where
 
 import Util ( sort, splitOn )
 
--- $> main01
 main01 :: IO ()
 main01 = do
-    input <- map (map read) . splitOn [""] . lines <$> readFile "res/input01" :: IO [[Int]]
+    input <- map (map read) . splitOn [""] . lines <$> readFile "res/input01"
     print $ (maximum . map sum) input
     print $ (sum . take 3 . reverse . sort . map sum) input
